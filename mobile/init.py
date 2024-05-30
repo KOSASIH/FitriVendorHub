ffb70841014006bb5baf0e1ad8c050e8a8ec2858
KOSASIH <kosasihg88@gmail.com>
2024-05-30 23:57:53 +0700

    Create init.py

---
+++ mobile/init.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from.models import db
+from.routes import api
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'qlite:///mobile.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    app.register_blueprint(api)
+    return app
