730fe5b140f4562fc7416aea2dd1dc99e11c5963
KOSASIH <kosasihg88@gmail.com>
2024-06-02 09:38:52 +0700

    Create Order.php

---
+++ app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    //... other code...
+
+    /**
+     * Get the status associated with the order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(OrderStatus::class)
+            ->with('details'); // Eager-load order status details
+    }
+
+    //... other code...
+}
